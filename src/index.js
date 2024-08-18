b0749d8737dbe5077af97a3e5b33e52ce98c1a5f
Serhii Savchenko <sergey.savchenko1303@gmail.com>
2024-08-18 16:05:47 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store.js';
+import App from 'components/App/App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+    <BrowserRouter basename="/Camper-Advert-App">
+      <App />
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
